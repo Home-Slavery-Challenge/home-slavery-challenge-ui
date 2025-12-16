42a317772865f1b7ff0d9aff4555a160c2c0a8ca
canse <jucansell@gmail.com>
2025-12-16 15:02:17 +0100

    :tada: Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet} from '@angular/router';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    HeaderComponent
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'slavery-home-challenge-ui';
 }
